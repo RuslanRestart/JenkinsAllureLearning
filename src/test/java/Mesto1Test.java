eaae23784d1f750b90ac48d359a0d616873dd066
RuslanRestart <erofeew.ruslan2012@yandex.ru>
2025-01-26 16:08:31 +0300

    Add incorrect test for demo

--- src/test/java/Mesto1Test.java
+++ src/test/java/Mesto1Test.java
@@ -30,6 +30,16 @@ public class Mesto1Test {
     }
 
     @Test
+    @DisplayName("Check user name")
+    @Description("This test is for check current user's name.")
+    public void checkUserName() {
+        given()
+                .auth().oauth2(bearerToken) // Передаём токен для аутентификации
+                .get("/api/users/me") // Делаем GET-запрос
+                .then().assertThat().body("data.name", equalTo("Incorrect Name")); // Проверяем, что имя соответствует ожидаемому
+    }
+
+    @Test
     @DisplayName("Like the first photo")
     @Description("This test is for liking the first photo on Mesto.")
     public void likeTheFirstPhoto() {
